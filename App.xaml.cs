a4b2251fb14ca0d303a5e9f296fef23af025012f
Steve Falcigno <steve1281@hotmail.com>
2025-06-22 06:18:49 -0400

    add code to create database if it doesn't exist.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,12 @@ namespace SQlite_Login_Example
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            DatabaseFacade facade = new DatabaseFacade(new UserDataContext());
+            facade.EnsureCreated();
+
+        }
     }
 
 }
