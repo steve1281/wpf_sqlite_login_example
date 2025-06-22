3fa2695a53c8912962efa9e56ba998675c6d21bf
Steve Falcigno <steve1281@hotmail.com>
2025-06-22 06:14:55 -0400

    add database interface and user class for our table

---
+++ User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SQlite_Login_Example
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+    }
+}
